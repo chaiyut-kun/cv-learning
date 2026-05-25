21ad81399e5e2f1e6f227c42e8bc5f8f8c9e8d85
chaiyut-kun <chwork28@hotmail.com>
2026-05-25 18:23:42 +0700

    ep2 of louis
    https://www.youtube.com/watch?v=mfiMUH0_90A&t=105s

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import cv2 as cv
+from ultralytics import YOLO
 
-img = cv.imread('./chalie.jpg')
-face_model = cv.CascadeClassifier('./face-detection.xml')
-gray_scale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-faces = face_model.detectMultiScale(gray_scale)
+# mouse callback function to get image coordinate
+def mouse_callback(event, x, y, flags, param):
+    if event == cv.EVENT_LBUTTONDOWN:
+        # convert scaled coordinates to original
+        orgin_x = int(x / ratio)
+        orgin_y = int(y / ratio)
+        print(f"Mouse clicked at: X={orgin_x}, Y={orgin_y}")
 
-for (x, y, w, h) in faces:
-    cv.rectangle(img, (x, y), (x+w, y+h), (255,255,0), 2)
+def get_lane_divider_x(y):
+    """Draw sloped lane divider based on curb"""
+    return int(lane_divider_slope * y + lane_divider_intercept)
 
-cv.imshow('image', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+def draw_sloped_lane_divider(frame, y_start, y_end):
+    x_start = get_lane_divider_x(y_start)
+    x_end = get_lane_divider_x(y_end)
+    cv.line(frame, (x_start, y_start), (x_end, y_end), (255,255,255), 3)
+
+
+ratio = 0.5
+line_y_in = 1300
+line_y_out = line_y_in
+lane_divider_slope = 0.409
+lane_divider_intercept = 1459.6
+divider_x_at_out = get_lane_divider_x(line_y_out)
+divider_x_at_in = get_lane_divider_x(line_y_in)
