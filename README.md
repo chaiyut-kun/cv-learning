3477bb60c876112ff6d268b25c7af3572086712d
chaiyut-kun <chwork28@hotmail.com>
2026-05-26 10:50:10 +0700

    track object with id and class name

---
+++ README.md
@@ -0,0 +1,3 @@
+# Louis MakerLab
+
+[Youtube](https://www.youtube.com/watch?v=mfiMUH0_90A&t=105s)
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def mouse_callback(event, x, y, flags, param):
 def get_lane_divider_x(y):
     """Draw sloped lane divider based on curb"""
     return int(lane_divider_slope * y + lane_divider_intercept)
+    # สมการเส้นตรง y = mx + B (เคสนี้คือ x = my + B)
 
 def draw_sloped_lane_divider(frame, y_start, y_end):
     x_start = get_lane_divider_x(y_start)
@@ -26,3 +27,48 @@ lane_divider_slope = 0.409
 lane_divider_intercept = 1459.6
 divider_x_at_out = get_lane_divider_x(line_y_out)
 divider_x_at_in = get_lane_divider_x(line_y_in)
+
+# Load YOLO Model
+model = YOLO('yolo12l.pt')
+class_list = model.names
+
+# อ่าน vdo
+cap = cv.VideoCapture('vehicle-count.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Video has issue")
+        break
+    
+    # cal new height for scale down resolution
+    new_height = int(frame.shape[0] * ratio)
+    new_width = int(frame.shape[1] * ratio)
+    
+    # persist True = แทรกตัววัตถุไปเรื่อยๆ
+    results = model.track(frame, persist=True, classes=[2, 7], device='cpu', verbose=False)
+    if results[0].boxes.data is not None:
+        
+        result = results[0].boxes
+        
+        boxes = result.xyxy.cpu()
+        track_ids = result.id.int().cpu().tolist()
+        class_indices = result.cls.int().cpu().tolist()
+        confidences = result.conf.cpu()
+        
+    for box, track_id, class_idx, conf in zip(boxes, track_ids, class_indices, confidences):
+        x1, y1, x2, y2 = map(int, box)
+
+        class_name = class_list[class_idx]
+        cv.putText(frame, f"ID: {track_id} - {class_name}", (x1, y1 - 10), cv.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255))
+        
+        cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    
+    scaled_frame = cv.resize(frame, (new_width, new_height))
+    cv.imshow("frame of vdo", scaled_frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
